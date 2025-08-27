7e820c68251eecbcf9ab3d677eae79f107c24d8f
Jav <javarruedas@gmail.com>
2025-08-27 13:35:09 +0800

    commit stage

---
+++ aspnetapp/Model/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace aspnetapp.Model
+{
+    public class Person
+    {
+        string name;
+        public string Name { get { return name; } set { name = value; } }
+
+        string position;
+        public string Position { get { return position; } set { position = value; } }   
+
+    }
+}
